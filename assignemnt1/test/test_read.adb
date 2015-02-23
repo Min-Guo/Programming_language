dc07aedeeb0c983f5a1b6d1ea96ab4a471feaa39
Min Guo <xmguomin@gmail.com>
2015-02-23 16:40:32 -0500

    Modified test_read

--- assignemnt1/test/test_read.adb
+++ assignemnt1/test/test_read.adb
@@ -1,20 +1,57 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 
-procedure Test_Read is
+procedure Read1 is
 
-   Input_File    : File_Type;
-   Index         : Integer;
+type MatrixBuffer1 is 
+array (1 .. size * size) of Integer;
 
 begin
 
-   Ada.Text_IO.Open (File => Input_File, Mode => Ada.Text_IO.In_File, Name => "input.txt");
-   declare
-   Line : String := Ada.Text_IO.Get_Line (Input_File);
-   begin
-   Put(Line);
-   end;
-   --Ada.Text_IO.Put (Item => in Num);
-   Ada.Text_IO.Close (File => Input_File);
+	for i in 1 .. size * size loop
+		Ada.Integer_Text_IO.get(i);
+		MatrixBuffer1(i) := Ada.Integer_Text_TO.get(i);
+		i := i + 1;
+	end loop;
+	
+	for i in 0 .. (size -1) loop
+		for j in 1 .. size loop
+			FirstMatrix(i, j) := MatrixBuffer1(i * size + j);
+		end loop;
+	end loop; 
 
-end Test_Read;
+end Read1;
+
+
+
+procedure Read2 is
+
+type MatrixBuffer2 is 
+array (1 .. size * size) of Integer;
+
+begin
+
+	for i in 1 .. size * size loop
+		Ada.Integer_Text_IO.get(i);
+		MatrixBuffer2(i) := Ada.Integer_Text_TO.get(i);
+		i := i + 1;
+	end loop;
+	
+	for i in 0 .. (size -1) loop
+		for j in 1 .. size loop
+			SecondMatrix(i, j) := MatrixBuffer2(i * size + j);
+		end loop;
+	end loop; 
+
+end Read1;
+
+
+procedure Printer is
+
+begin 
+	for i in 1 .. size loop
+		put(ThridMatrix(1 .. size, i));
+	end loop
+end Printer;
+	
+
+
