ef5dd9662dd7ce8c888ac6db93b5422a37f80b6c
Min Guo <xmguomin@gmail.com>
2015-05-12 01:48:22 -0400

    Add scala assignment

--- HW2/code/Part2/src/Part2.scala
+++ HW2/code/Part2/src/Part2.scala
@@ -1,17 +1,16 @@
 
-class A  extends Ordered[A] {
-  var a : Int = 0
-  def this(value: Int) = {
-    this();
-    this.a = value;
-  }
-  def compare(that: A) = this.a - that.a
+class A(a:Int)  extends Ordered[A] {
+  var x  = a
+  def sum = x
+  def compare(that: A) = this.sum - that.sum
+  override def toString() = "A<" + x + ">"
 }
 
-class B extends A {
-  var b : Int = 0
-  var c: Int = 0
-  def sum(that: B) = that.c + that.b
+class B(b:Int, c: Int) extends A(b)  {
+  var y = b
+  var z = c
+  override def sum = z + y
+  override  def toString() = "B<" + y + "," + z + ">"
 }
 
 class C[+T] (g: Int => T){
@@ -25,31 +24,63 @@ abstract class Tree[T <: Ordered[T]] {
 case class Leaf[T <: Ordered[T]] (v: T) extends Tree[T] {
   override def insert(x: T): Tree[T] = {
     if (this.v < x) {
-      return Node[T](x, Leaf(v), Empty())
+       Node[T](v, Empty(), Leaf(x))
     } else {
-      return Node[T](v, Leaf(x), Empty())
+       Node[T](x, Empty(), Leaf(v))
     }
   }
 }
 case class Node[T <: Ordered[T]] (v: T, l:Tree[T], r:Tree[T]) extends Tree[T] {
+
   override def insert(x:T): Tree[T] = {
     if (this.v < x) {
-      this.r.insert(x)
+      Node[T](v, l, r.insert(x))
     } else {
-      this.l.insert(x)
+      Node[T](v, l.insert(x), r)
     }
   }
 }
 case class Empty[T <: Ordered[T]]() extends Tree[T] {
   override def insert (x:T): Tree[T] = {
-    return Leaf(x)
+     Leaf(x)
   }
 }
 
 
 object Part2 {
+  def applyTo10(c: C[A]) = c.apply(10)
   def main(args: Array[String]) {
-
+    test()
   }
+  def printTree(t: Tree[A]) {
+    t match {
+      case Node(v, l, r) => print("(");
+        printTree(l);
+        print(v);
+        printTree(r);
+        print(")");
+      case Leaf(v) => print(v);
+      case Empty() => ();
+    }
+  }
+  def test() {
+    val c1 = new C((x:Int) => new A(x))
+    println(c1.apply(3))
+
+    val c2 = new C((x:Int) => new B(x+1, x+2))
+    println(c2.apply(3))
+
+    println(applyTo10(c1))
+    println(applyTo10(c2))  //relies on covariant subtyping
+
+
+    var t1: Tree[A] = Empty()
+    t1 = t1.insert(new A(4))
+    t1 = t1.insert(new A(3))
+    t1 = t1.insert(new B(4,1))
+    t1 = t1.insert(new A(2))
+    printTree(t1)
+  }
+
 }
 
